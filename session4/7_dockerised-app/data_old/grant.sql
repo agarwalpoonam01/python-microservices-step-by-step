9c1f99fe80e71a4d9397efb6353fcbd4854bdc84
Poonam <poonam@Hrikss-MacBook-Air.local>
2022-11-28 17:24:49 +0530

    removing data folders of databse

--- session4/7_dockerised-app/data_old/grant.sql
+++
@@ -1,5 +0,0 @@
-create database users_db;
-CREATE USER 'mydemouser'@localhost IDENTIFIED BY 'demopassword';
-#GRANT ALL PRIVILEGES ON user_db.* TO mydemouser@localhost;
-GRANT ALL PRIVILEGES ON user_db.* TO 'mydemouser'@'%' identified by 'demopassword';
-FLUSH PRIVILEGES;
